d34b636cc3649150436a64909f0a7beb64c6a923
Damokless <damokless@protonmail.com>
2021-10-08 08:36:11 +0200

    first route done

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/cars")
+def getCars():
+    return "<p>all cars</p>"
