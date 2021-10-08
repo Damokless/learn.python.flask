d9e977b90a4b45ff92253596fcab7baa4fefc17d
Damokless <damokless@protonmail.com>
2021-10-08 10:14:12 +0200

    route delete cars by id done

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from os import name
 from flask import Flask, jsonify
 import json
 app = Flask(__name__)
@@ -9,13 +10,17 @@ with open('./assets/cars.json') as cars:
   def hello_world():
     return "<p>Hello, World!</p>"
   
-  @app.route("/api/cars")
+  @app.route("/api/cars", methods=['GET'])
   def getCars():
-        return jsonify(data)
+    return jsonify(data)
 
-  @app.route("/api/cars/id/<int:id>")
+  @app.route("/api/cars/id/<int:id>", methods=['GET'])
   def getCarsByID(id):
-      if id:
-          return data[id]
-      else:
-          return "<p>No car with this id</p>"
+    return data[id]
+
+  @app.route("/api/cars/delete/<int:id>", methods=['GET','DELETE'])
+  def deleteCarsByID(id):
+      del data[id]
+      with open('./assets/cars.json', "w") as json_file:
+        json.dump(data, json_file, indent=4)
+        return 'Book deleted'
